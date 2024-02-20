b3b0e95d071e954fd48b4133ded181bb7fe14210
ejfjfqpfp1f <160572154+ejfjfqpfp1f@users.noreply.github.com>
2024-02-20 16:22:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask,request,render_template
 
+app=Flask(_name_)
+
+@app,route("/",methods=["GET","POST"])
+def index();
+ return(render_template("index,html"))
+
+@app.route("/main",methods=["GET","POST"])
+ def main（）：
+name=request,form,get("name")
+return(rander_templates("main,html",r=name))
+
+if_name_=="_main_";
+ app.run()
