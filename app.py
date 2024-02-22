a4f97a069e7e6d054643366c48ec2c0fdfcf2886
ejfjfqpfp1f <160572154+ejfjfqpfp1f@users.noreply.github.com>
2024-02-22 10:36:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import os
 import time
 from openai import OpenAI
 
+open_api_key=os.environ["OpenAI_API_TOKEN"]
 os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
+model = OpenAI(api_key=open_api_key)
+
 app = Flask(__name__)
+
 
 r = ""
 first_time = 1
@@ -21,6 +25,23 @@ def main():
         r = request.form.get("r")
         first_time=0
     return(render_template("main.html",r=r))
+
+@app.route("/text_gpt",methods=["GET","POST"])
+def text_gpt():
+    return(render_template("text_gpt.html")
+
+@app.route("/text_result",methods=["GET","POST"])
+def text_result():
+    q=request.form.get("q")
+    r=model.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages=[
+            "role" : "user",
+            "content" : q
+        ]
+    )
+    time.sleep(5)
+    return(render_template("text_result.html",r=r.choices[0].message.content))
 
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
@@ -42,7 +63,7 @@ def image_result():
 def end():
     global first_time
     first_time = 1
-    return(render_template("end.html"))
+    return(render_template("end.html"),r=r)
 
 if __name__ == "__main__":
     app.run()
